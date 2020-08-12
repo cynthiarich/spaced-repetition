e46eaae772b07e182101acf234365bbfa2a1f7c6
Cynthia Rich <crichID@github.com>
2020-08-12 03:49:17 -0400

    switch to cleardb

--- models/index.js
+++ models/index.js
@@ -10,8 +10,8 @@ const db = {};
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
